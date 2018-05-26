4bd7dd312c10975873f06c7d75056a1af5676856
stacief510 <staciemfraser@gmail.com>
2018-05-25 17:27:52 -0700

    created destroy function for one drink review

--- controllers/drinks.js
+++ controllers/drinks.js
@@ -23,8 +23,8 @@ function create(req, res){
 
 function show(req, res){
 	console.log('GET one drink')
-	console.log(`req.params.id: ${req.params.id}`);
-	db.Drink.findById(req.params.id, function(err,foundDrink){
+	console.log(`req.params.id: ${req.params.drink_id}`);
+	db.Drink.findById(req.params.drink_id, function(err,foundDrink){
 		res.json(foundDrink);
 	});
 }
@@ -34,7 +34,13 @@ function update(req, res){
 }
 
 function destroy(req, res){
-
+    console.log('Deleting a drink...')
+    db.Drink.findByIdAndRemove(req.params.drink_id, function(err,foundDrink){
+        if (err){
+            console.log(err);
+        }
+		res.send("drink review deleted");
+	});
 }
 
 module.exports = {
